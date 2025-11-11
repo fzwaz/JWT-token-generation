8faf66853c13f44e9ff24271187dfae7a39b2302
Fawaz <fawaz.muhmmd@gmail.com>
2025-11-11 16:08:59 +0530

    base file

---
+++ index.js
@@ -0,0 +1,49 @@
+const jwt = require("jsonwebtoken");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+const PORT = 3000;
+
+const users = [];
+
+app.post('/register',async (req,res)=>{
+    const {username , password}= req.body;
+
+    if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+    }
+    const existingUser = users.find(u=>u.username === username);
+    if(!existingUser){
+        return res.status(400).json({ message: 'Username already exists' });
+    }
+
+    const hashPassword = await bcrypt.hash(password,10);
+
+    const newUser = {
+        id : users.length + 1,
+        username,
+        password: hashPassword,
+    };
+
+    users.push(newUser);
+
+    res.status(201).json({message:"user registered successfully!"});
+});
+
+app.post('/login',async (req,res)=>{
+    const {username , password}=req.body;
+
+    const user = users.find(u=>u.username===username);
+    if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+
+    const isValid = await bcrypt.compare(password , user.password);
+    if (!isValid) return res.status(401).json({ message: 'Invalid credentials' });
+
+    
+});
